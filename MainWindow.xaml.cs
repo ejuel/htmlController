ed3b8fdf2eae0c1b4d2a62a4d791762af9cf0e81
ejuel_Dell <10731882+zeke225@users.noreply.github.com>
2020-08-15 09:21:16 -0500

    WPF Host
    Initial commit for HTML Controller

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿// For roadmap of planned features, see: https://docs.google.com/document/d/1kg3DW67yjs3DcU9kXiGIHd91lLocttz44sFLUwcJQzk/edit?usp=sharing
+
+using System.Threading;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace htmlController
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            while (true)
+            {
+                Thread.Sleep(5000);
+                SendKeys.SendWait("A"); //Testing user inputs
+            }
+        }
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            SendKeys.Send("A");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+//using System.Windows.Forms
+namespace htmlController
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Thread.Sleep(10000);
+                Console.WriteLine(DateTime.Now.ToString("F"));
+                //object.SendKeys.
+            }
+        }
+    }
+}
